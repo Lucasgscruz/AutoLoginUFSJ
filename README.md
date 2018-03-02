4bee18b84c0628b57f609d1680b82057b5d95322
exterminus <cmagnobarbosa@gmail.com>
2018-03-01 21:21:48 -0300

    Sistema de AutoLogin para Internet da UFSJ v0.1

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # AutoLoginUFSJ
 AutoLogin para sistema de autentificação da UFSJ
+
+* Em desenvolvimento..
+* Funcional.
+
+
+# Requisitos
+* Biblioteca Selenium..
+* GeckoDriver - Firefox
+
+Instalando o Selenium
+ > pip install -U selenium
+
+Instalando o Driver..
+
+Baixe o drive em:
+> https://github.com/mozilla/geckodriver/releases/download/v0.19.1/geckodriver-v0.19.1-linux64.tar.gz
+
+Descompacte o geckodriver e modifique a permissão de execução do mesmo:
+> chmod +x geckodriver
+
+Agora mova o arquivo para o diretorio:
+> /usr/local/bin
+
+
+# Configure o seu login
+
+Abra o auto.py e configure:
+
+* cpf(Cpf usado para acessar a intranet)
+* Senha
+
+# Como usuar
+
+Execute no terminal :
+>python auto.py
+
+Execute antes de realizar o login..
+
+# Como Ajudar..
+
+Crie novas versões melhorando a portabilidade e usabilidade.
---
+++ auto.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# # -*- coding: UTF-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+"""
+AutoLogin 0.1
+Necessario a biblioteca selenium...
+Carlos Magno
+UFSJ
+Referência: https://stackoverflow.com/
+questions/21186327/fill-username-and-
+password-using-selenium-in-python
+"""
+# Configuração de Login e senha.
+cpf = "Meu_CPF"
+senha = "Minha_senha"
+# Realiza o login na pagina.
+
+
+def logar():
+    navegador = webdriver.Firefox()
+    # site base para abrir a pagina de login
+    navegador.get("http://g1.com.br")
+    # Id do Login
+    username = navegador.find_element_by_id("ft_un")
+    # Id da Senha
+    senha = navegador.find_element_by_id("ft_pd")
+    # usuario
+    username.send_keys(cpf)
+    # senha
+    senha.send_keys(senha)
+    senha.send_keys(Keys.ENTER)
+    # fecha a pagina do navegador
+    navegador.close()
+
+while(True):
+    logar()
+    # tempo até o proximo login
+    time.sleep(961)
