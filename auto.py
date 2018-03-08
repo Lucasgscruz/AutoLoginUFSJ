1d5afe4455dc48e8ec31c40bdea5d8056bfe7e5b
exterminus <cmagnobarbosa@gmail.com>
2018-03-08 18:43:19 -0300

    Melhorias autologin

--- auto.py
+++ auto.py
@@ -3,7 +3,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-
+import urllib2
 """
 AutoLogin 0.1
 Necessario a biblioteca selenium...
@@ -19,30 +19,48 @@ minhasenha = "senha"
 # Realiza o login na pagina.
 
 
+def realizar_conexao():
+	try:
+		#link para verificar a conexão..
+		f=urllib2.urlopen('https://www.google.com.br',timeout=1)
+		#ja conectado
+		#print "Já conectado"
+		return False
+	except Exception as e:
+		#Requisitar conexão
+		#print "erro",e
+		return True
 def logar():
-    navegador = webdriver.Firefox()
-    # site base para abrir a pagina de login
-    navegador.get("http://g1.com.br")
-    # Id do Login
-    username = navegador.find_element_by_id("ft_un")
-    # Id da Senha
-    senha = navegador.find_element_by_id("ft_pd")
-    # usuario
-    username.send_keys(cpf)
-    # senha
-    senha.send_keys(minhasenha)
-    senha.send_keys(Keys.ENTER)
-    # fecha a pagina do navegador
-    navegador.close()
+	try:
+		navegador = webdriver.Firefox()
+		navegador.get("http://g1.com.br")
+		#procura os campos correspondentes em html...
+		username = navegador.find_element_by_id("ft_un")
+		senha = navegador.find_element_by_id("ft_pd")
+		#envia o cpf para a pagina
+		username.send_keys(cpf)
+		#envia a senha
+		senha.send_keys(minhasenha)
+		#simula o enter do navegador..
+		senha.send_keys(Keys.ENTER)
+	except Exception as e:
+		print "Erro na conexão"
+		navegador.close()
+		return False
+
+	navegador.close()
+	return True
 
 while(True):
-	try:
-		logar()
-	except Exception as e:
-		while(rodar):
-			print "Erro na revalidação de autentificação :)."
-		exit()
-   
-    # tempo até o proximo login
-    print ("Executando.. aguardando a próxima autentificação.")
-    time.sleep(1500)
+	#verifica a permissão para realizar a conexão
+	if realizar_conexao():
+		print "Realizando conexao"
+		if(logar()):
+			print "Conectado"
+		else:
+			print "Erro no Login"
+
+	else:
+		print "Conexao Ok, verifição automática em 5 minutos"
+		#DORME POR 5 MINUTOS
+		time.sleep(300)
