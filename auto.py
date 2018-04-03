331f49cbe73e44ad3bce55835219aba5db7e8015
exterminus <cmagnobarbosa@gmail.com>
2018-04-03 11:45:30 -0300

    correcao de erros

--- auto.py
+++ auto.py
@@ -13,15 +13,10 @@ Referência: https://stackoverflow.com/
 questions/21186327/fill-username-and-
 password-using-selenium-in-python
 """
-# Configuração de Login e senha.
 cpf = "CPF"
 minhasenha = "senha"
-# Realiza o login na pagina.
-
-
 def realizar_conexao():
 	try:
-		#link para verificar a conexão..
 		f=urllib2.urlopen('https://www.google.com.br',timeout=1)
 		#ja conectado
 		#print "Já conectado"
@@ -34,14 +29,10 @@ def logar():
 	try:
 		navegador = webdriver.Firefox()
 		navegador.get("http://g1.com.br")
-		#procura os campos correspondentes em html...
 		username = navegador.find_element_by_id("ft_un")
 		senha = navegador.find_element_by_id("ft_pd")
-		#envia o cpf para a pagina
 		username.send_keys(cpf)
-		#envia a senha
 		senha.send_keys(minhasenha)
-		#simula o enter do navegador..
 		senha.send_keys(Keys.ENTER)
 	except Exception as e:
 		print "Erro na conexão"
